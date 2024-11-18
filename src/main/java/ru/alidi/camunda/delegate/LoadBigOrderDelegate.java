89fd96cb08c3b7b983f0e08c7a6df2f4dcb43d08
a.fedoseev <molefed@gmail.com>
2024-11-18 17:19:54 +0500

    Error start

--- src/main/java/ru/alidi/camunda/delegate/LoadBigOrderDelegate.java
+++ src/main/java/ru/alidi/camunda/delegate/LoadBigOrderDelegate.java
@@ -10,6 +10,11 @@ public class LoadBigOrderDelegate implements JavaDelegate {
         var var = (int) execution.getVariable("ORDER_ITEMS_COUNT");
 
         System.out.println("Заказ большой. %s элементов.".formatted(var));
+
+
+//        if (var < 0) {
+//            execution.createIncident("LOAD_BIG_ORDER_ERROR", "Какая-то ошибка");
+//        }
     }
 
 }
